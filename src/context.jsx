271b8143d706da34336586e5e6f87933ac26d899
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-05 11:32:02 -0500

    userv prefers Dark mode. local storage

--- src/context.jsx
+++ src/context.jsx
@@ -2,16 +2,28 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "prefers-color-scheme:dark"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme");
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+  return storedDarkMode === "true";
+};
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode);
   const [searchImage, setSearchImage] = useState("House");
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newDarkTheme);
-    console.log(body);
+    localStorage.setItem("darkTheme", newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchImage, setSearchImage }}
